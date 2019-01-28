c88874554c55bbd2de921d7daa0bb96331f92d6a
Cmoney <cascherer42@gmail.com>
2019-01-28 15:11:55 -0600

    Updates

---
+++ precache-manifest.d332d4ddde70c87cbfe51866ac279cfb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "efcd2a97755b4cf1991a",
+    "url": "/BarrensChatGenerator/static/js/runtime~main.efcd2a97.js"
+  },
+  {
+    "revision": "82da03780953cfc3d42e",
+    "url": "/BarrensChatGenerator/static/js/main.82da0378.chunk.js"
+  },
+  {
+    "revision": "9b0cdc6058af31392568",
+    "url": "/BarrensChatGenerator/static/js/1.9b0cdc60.chunk.js"
+  },
+  {
+    "revision": "82da03780953cfc3d42e",
+    "url": "/BarrensChatGenerator/static/css/main.fda80ce0.chunk.css"
+  },
+  {
+    "revision": "4d41741d8f6ab6e8f8241fe6aaeb519b",
+    "url": "/BarrensChatGenerator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-portfolio/precache-manifest.3b1df4ab95caf67cbe9fa6a0da7865da.js"
+  "/BarrensChatGenerator/precache-manifest.d332d4ddde70c87cbfe51866ac279cfb.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/React-portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/BarrensChatGenerator/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
