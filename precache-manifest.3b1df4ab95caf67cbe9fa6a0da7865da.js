730adf30a863951b791a9e7b990245152fb92b19
Cmoney <cascherer42@gmail.com>
2019-01-28 14:20:09 -0600

    Updates

---
+++ precache-manifest.3b1df4ab95caf67cbe9fa6a0da7865da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a21001861bb46a901746",
+    "url": "/React-portfolio/static/js/runtime~main.a2100186.js"
+  },
+  {
+    "revision": "85357689fc2487cbe83c",
+    "url": "/React-portfolio/static/js/main.85357689.chunk.js"
+  },
+  {
+    "revision": "9b0cdc6058af31392568",
+    "url": "/React-portfolio/static/js/1.9b0cdc60.chunk.js"
+  },
+  {
+    "revision": "85357689fc2487cbe83c",
+    "url": "/React-portfolio/static/css/main.fda80ce0.chunk.css"
+  },
+  {
+    "revision": "ef8318b76cad43d40cd23bf1c08bce68",
+    "url": "/React-portfolio/index.html"
+  }
+];
